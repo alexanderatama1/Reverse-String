96029b3ed6d00b011fcd4112f9567a8fc3ad6bb8
Anthony Alexander Atama <100134398+alexanderatama1@users.noreply.github.com>
2023-11-12 12:53:50 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Declare variables to store input and reversed output
+string input, output = "";
+
+// Prompt the user to enter a string
+Console.Write("Enter a string: ");
+
+// Read the user input and store it in the 'input' variable
+input = Console.ReadLine();
+
+// Iterate through each character in the input string in reverse order
+for (int i = input.Length - 1; i >= 0; i--)
+{
+    // Concatenate each character to the 'output' string, effectively reversing the string
+    output += input[i];
+}
+
+// Display the reversed string using string interpolation
+Console.WriteLine($"Reversed is {output}");
